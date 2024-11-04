690579654dc7052ff6101f52dea2ea78ad063de3
Andreas <102608406+andytandy24@users.noreply.github.com>
2024-11-04 17:47:46 +0100

    Update blockySIGN.py

--- blockySIGN.py
+++ blockySIGN.py
@@ -1,30 +1,10 @@
 import pyshark
 import argparse
 
-# Converts 64 bit to coordinates 
-'''
-def split_coordinates(value):
-    x = (value >> 38) & 0x3FFFFFF 
-    z = (value >> 12) & 0x3FFFFFF 
-    y = value & 0xFFF 
-
-    if x & (1 << 25):
-        x -= (1 << 26)
-    
-    if z & (1 << 25):
-        z -= (1 << 26)
-
-    if y & (1 << 11): 
-        y -= (1 << 12)
-
-    return [x, z, y]
-'''
-
 # CLI
 parser = argparse.ArgumentParser(description="This script finds spesific events in a minecraft PCAP/PCAPNG recording. \nGo to wiki.vg/Protocol to find the event you want to find")
 parser.add_argument("-i", "--input", type=str, help="Input file path of PCAP/PCAPNG file")
 parser.add_argument("-P", "--packetId", type=str, help="Packet ID of the desired packet")
-parser.add_argument("-o", "--output", type=str, default="output.txt", help="Path of output file")
 args = parser.parse_args()
 
 inputFile = args.input
